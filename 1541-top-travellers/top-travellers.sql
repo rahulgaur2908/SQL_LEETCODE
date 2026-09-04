2b5f070cd8a6ac595b009886c3be824275a81603
Rahul Gaur <rahulgaur0102@gmail.com>
2026-09-04 21:28:50 +0530

    Time: 869 ms (73.37%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+SELECT DISTINCT
+u.name,
+IFNULL(
+    SUM(t.distance) OVER (PARTITION BY u.id), 0) AS travelled_distance
+FROM
+Users AS u
+LEFT JOIN
+Rides AS t
+ON 
+u.id = t.user_id
+ORDER BY travelled_distance DESC, u.name ASC;
