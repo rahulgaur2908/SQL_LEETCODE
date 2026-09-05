7ba661192e3506d0f909ac57cd9f969f0185e0f1
Rahul Gaur <rahulgaur0102@gmail.com>
2026-09-05 11:48:27 +0530

    Time: 624 ms (68.51%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+
+SELECT employee_id, salary AS bonus
+FROM employees
+WHERE employee_id % 2 <> 0 AND name NOT LIKE "M%"
+
+UNION
+
+SELECT employee_id, 0 AS bonus
+FROM employees
+WHERE employee_id % 2 = 0 OR name LIKE "M%"
+ORDER BY employee_id
