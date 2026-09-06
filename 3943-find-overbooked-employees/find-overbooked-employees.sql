b59a4bcd9d1f722680bff1270af4cbf37d879a59
Rahul Gaur <rahulgaur0102@gmail.com>
2026-09-06 18:09:45 +0530

    Time: 630 ms (53.35%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3943-find-overbooked-employees/find-overbooked-employees.sql
@@ -0,0 +1,22 @@
+# Write your MySQL query statement below
+
+WITH employee_hour AS (
+    SELECT employee_id, DATEDIFF(meeting_date, "1900-01-01") DIV 7 AS w,
+    SUM(duration_hours) AS hours FROM meetings GROUP BY employee_id, w
+),
+
+employee_heavy AS (
+    SELECT employee_id, COUNT(*) AS meeting_heavy_weeks FROM employee_hour
+    WHERE hours > 20 GROUP BY employee_id
+)
+
+SELECT a.employee_id, a.employee_name, a.department, b.meeting_heavy_weeks
+FROM
+employees AS a,
+employee_heavy AS b
+WHERE
+a.employee_id = b.employee_id
+AND
+b.meeting_heavy_weeks > 1
+ORDER BY
+b.meeting_heavy_weeks DESC, a.employee_name
